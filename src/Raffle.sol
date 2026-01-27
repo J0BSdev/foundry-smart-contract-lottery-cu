cead96c7f35d1465724c3e5bd4847472a2262511
Lovro <lovro.posel79@gmail.com>
2026-01-27 19:47:14 +0100

    update

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -173,4 +173,10 @@ function getRaffleState() public view returns (RaffleState){
   return s_raffleState;
 
 }
+
+function getPlayer(uint256 index) external view returns (address){
+
+  return s_players[index];
+
+}
 }
--- test/unit/Raffle.t.sol
+++ test/unit/Raffle.t.sol
@@ -19,7 +19,11 @@ uint256 entranceFee;
         address link;
 
 address public PLAYER = makeAddr("player");
-uint256 public constant STARTING_USER_BALANCE = 10 ether;
+uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+
+ event RaffleEntered(address indexed player);
+event WinnerPicked(address indexed winner);
 
 
 
@@ -36,11 +40,43 @@ uint256 public constant STARTING_USER_BALANCE = 10 ether;
         link = networkConfig.link;
 
 
+        vm.deal(PLAYER, STARTING_PLAYER_BALANCE);
     
     }
-
-
 function testRaffleInitializesInOpenState() public view {
     assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+}
+
+function testRaffleRevertsWhenYouDontPayEnough() public{
+    //Arrange
+    vm.prank(PLAYER);
+    //Act/assertt
+    vm.expectRevert(Raffle.Raffle__SendMoretoEnterRaffle.selector);
+    raffle.enterRaffle();
+    //Assert
+
+}
+function testRaffleRevertsWhenYouPayLessThanTheEntranceFee() public{
+    //Arrange
+    vm.prank(PLAYER);
+    //Act/assert
+   address playerRecorded = raffle.getPlayer(0);
+   assert(playerRecorded == PLAYER);
+    //Assert
+}
+
+
+
+
+
+
+function testRaffleEmitsEventOnEntrance() public{
+//Arrange
+vm.prank(PLAYER);
+//act
+vm.expectEmit(true, false, false, false, address(raffle));
+emit RaffleEntered(PLAYER);
+raffle.enterRaffle{value: entranceFee}();
+//assert
 }
 }
